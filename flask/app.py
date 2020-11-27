37e1537dfb2979e6b8e62e3fc54e50c4994238d3
Katechi <katerinne.leiva@gmail.com>
2020-11-27 12:28:36 -0300

    Actualización Proyecto con Flask
    Borre la antigua carpeta de flask y pegue otra nueva con todo actualizado

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 import csv
 
+import json
+
+from flask import request
+
 def read_csv_file(filename):
     data = []
     for row in csv.reader(open(filename)):
@@ -18,13 +22,23 @@ def procesar (data):
         y.append(float(data[fila][6]))
         z.append(float(data[fila][7]))
         k.append(float(data[fila][13]))
-    print (y)
+    #print (y)
     return x,y,z ,k
+
+def latlong (data):
+    ltln = []
+    for fila in range(1, len(data)):
+        lat=(float(data[fila][2]))
+        lon=(float(data[fila][3])) 
+        ltln.append([lat,lon])
+    
+    return ltln
     
 
 
 datos         = read_csv_file('uma_02.csv')
 x,y,z,k = procesar(datos)
+latlon = latlong(datos)
 
 
 app = Flask(__name__)
@@ -32,6 +46,20 @@ app = Flask(__name__)
 @app.route('/')
 def template():
     return render_template('index.html',x= x,y = y, z=z , k=k)
+
+@app.route('/mapa')
+def mapa():
+    return render_template('mapa.html',latlon=latlon)
+
+@app.route('/grafi')
+def grafi():
+    return render_template('grafi.html',latlon=latlon)
+
+@app.route('/json', methods=['GET', 'POST'])
+def parse_request():
+    data = request.data 
+    dataDict = json.loads(data)
+    return render_template('grafi.html')
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000,host='127.0.0.1')
